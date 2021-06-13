e6fd57e18431b559c568300286f4d6f445ff5db6
KishoremrK <83497293+KishoremrK@users.noreply.github.com>
2021-06-13 18:11:05 +0400

    Update easy.js

--- static/js/easy.js
+++ static/js/easy.js
@@ -66,7 +66,6 @@ function resettimer()
 function reload()
 {sqshuffle();
  shuffle();
- leaderboard();
  document.getElementById("mov") .innerHTML= 0; 
  document.getElementById("win").style.display = "none";
  move=0; totalSeconds = 0;
@@ -187,7 +186,6 @@ function issolved()
  document.getElementById("message1").innerText=" MOVES : "+move ;
  document.getElementById("message2").innerText="TIME : "+min+":"+sec;  
  var s=score(min,sec,move);
- topscorer(s); 
  document.getElementById("message").innerText=" SCORE : "+s; 
  } 
 
@@ -218,25 +216,4 @@ function pad(val)
    else
      { return valString; }
 }
-
-function topscorer(currentscore)
-{
-  var highscore = localStorage.getItem("topscore");
-  if (currentscore >highscore)
-    { localStorage.setItem("topscore",currentscore);
-      var name = prompt("ENTER YOUR NAME");
-      localStorage.setItem("topscorername",name);
-      document.getElementById("message4").innerText=" YOU ARE THE TOP SCORER ";          }
-  else
-  { document.getElementById("message4").innerText=" TOP SCORE : "+highscore; }
-}
-
-function leaderboard()
-{ var x,y;
-  x= localStorage.getItem("topscore");
-  y= localStorage.getItem("topscorername");
- document.getElementById("board") .innerHTML= "LEADERBOARD";
- document.getElementById("topper") .innerHTML= y.toUpperCase()+"-"+x;
-}
-
 
